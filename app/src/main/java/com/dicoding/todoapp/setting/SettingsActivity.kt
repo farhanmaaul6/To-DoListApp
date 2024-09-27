8b2549d0ec8f489e7f9204b5f1985d7bcd00c482
farhanmaaul6 <farhanmaulana111@gmail.com>
2024-09-27 23:08:09 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/todoapp/setting/SettingsActivity.kt
@@ -0,0 +1,61 @@
+package com.dicoding.todoapp.setting
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
+import androidx.work.*
+import com.dicoding.todoapp.R
+import com.dicoding.todoapp.notification.NotificationWorker
+import com.dicoding.todoapp.utils.NOTIFICATION_CHANNEL_ID
+import java.util.concurrent.TimeUnit
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            val prefNotification = findPreference<SwitchPreference>(getString(R.string.pref_key_notify))
+            prefNotification?.setOnPreferenceChangeListener { preference, newValue ->
+                val channelName = getString(R.string.notify_channel_name)
+                //TODO 13 : Schedule and cancel daily reminder using WorkManager with data channelName
+                val workManager = WorkManager.getInstance(requireContext())
+                if (newValue as Boolean) {
+                    val data = Data.Builder()
+                        .putString(NOTIFICATION_CHANNEL_ID, channelName)
+                        .build()
+
+                    val periodicWorkRequest =
+                        PeriodicWorkRequest.Builder(NotificationWorker::class.java,1, TimeUnit.DAYS)
+                            .setInputData(data)
+                            .build()
+
+                    workManager.enqueue(periodicWorkRequest)
+                }
+
+
+                true
+            }
+        }
+
+    }
+}
